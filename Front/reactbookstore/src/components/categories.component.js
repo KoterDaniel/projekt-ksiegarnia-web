8494f63a9a335eac308ac4a5120236af66ae2ab8
kodzi97 <64788254+kodzi97@users.noreply.github.com>
2021-05-18 14:17:49 +0200

    Add files via upload

--- Front/reactbookstore/src/components/categories.component.js
+++ Front/reactbookstore/src/components/categories.component.js
@@ -2,12 +2,23 @@ import React, { Component } from "react";
 
 import UserService from "../services/user.service";
 
+import book1 from "../assets/book1.jpg";
+import book2 from "../assets/book2.jpg";
+import book3 from "../assets/book3.jpg";
+import book4 from "../assets/book4.jpg";
+
 export default class Categories extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       content: "",
+      menuTitle: "Wybierz kategorie",
+      title: "Kategoria",
+      drama: "Drama",
+      fantasy: "Fantasy",
+      thriller: "Thriller",
+      inne: "Inne",
     };
   }
 
@@ -29,17 +40,41 @@ export default class Categories extends Component {
     );
   }
 
+  ChangeCategory = () => {
+    //var x = this.state.fantasy;
+
+    var x = document.getElementsByClassName("dropdown-item")[0];
+    this.setState({ menuTitle: x.textContent });
+    this.setState({ title: x.textContent });
+
+    var index;
+    var list = document.getElementsByClassName("book-item");
+
+    if (x.textContent === "Drama") {
+      for (index = 0; index < list.length; ++index) {
+        list[index].style.backgroundImage = `url(${book1})`;
+      }
+    } else if (x.textContent === "Fantasy") {
+      for (index = 0; index < list.length; ++index) {
+        list[index].style.backgroundImage = `url(${book2})`;
+      }
+    } else if (x.textContent === "Thriller") {
+      for (index = 0; index < list.length; ++index) {
+        list[index].style.backgroundImage = `url(${book3})`;
+      }
+    } else {
+      for (index = 0; index < list.length; ++index) {
+        list[index].style.backgroundImage = `url(${book4})`;
+      }
+    }
+  };
+
   render() {
     return (
-      <div className="container">
-        <header className="jumbotron">
-          <h3>Kategorie</h3>
-          <h3>{this.state.content}</h3>
-        </header>
-        <div class="dropdown show ml-3 mb-3">
-          {" "}
+      <div className="body">
+        <div className="dropdown show ml-3 mb-3">
           <a
-            class="btn btn-dark dropdown-toggle"
+            className="btn btn-dark dropdown-toggle w-25"
             href=""
             role="button"
             id="dropdownMenuLink"
@@ -47,21 +82,124 @@ export default class Categories extends Component {
             aria-haspopup="true"
             aria-expanded="false"
           >
-            {" "}
-            Wybierz Kategorie{" "}
-          </a>{" "}
-          <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-            {" "}
-            <a class="dropdown-item" href="#">
-              Drama
-            </a>{" "}
-            <a class="dropdown-item" href="#">
-              Fantasy
-            </a>{" "}
-            <a class="dropdown-item" href="#">
-              Thriller
-            </a>{" "}
-          </div>{" "}
+            <div className="mainText">{this.state.menuTitle}</div>
+          </a>
+
+          <div
+            onClick={this.ChangeCategory}
+            className="dropdown-menu"
+            aria-labelledby="dropdownMenuLink"
+          >
+            <li>
+              <a className="dropdown-item" href="#">
+                {this.state.drama}
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                {this.state.fantasy}
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                {this.state.thriller}
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                {this.state.inne}
+              </a>
+            </li>
+          </div>
+        </div>
+        <div className="category-title">
+          <h2 className="display-4 mb-4 mainText">
+            <strong>{this.state.title}</strong>
+          </h2>
+        </div>
+        <div className="row">
+          <div className="col-lg-12 col-md-10">
+            <div
+              className="book-item"
+              style={{ backgroundImage: `url(${book1})` }}
+            ></div>
+            <div
+              className="book-item"
+              style={{ backgroundImage: `url(${book2})` }}
+            ></div>
+            <div
+              className="book-item"
+              style={{ backgroundImage: `url(${book3})` }}
+            ></div>
+            <div
+              className="book-item"
+              style={{ backgroundImage: `url(${book4})` }}
+            ></div>
+            <div
+              className="book-item"
+              style={{ backgroundImage: `url(${book1})` }}
+            ></div>
+            <div
+              className="book-item"
+              style={{ backgroundImage: `url(${book2})` }}
+            ></div>
+            <div
+              className="book-item"
+              style={{ backgroundImage: `url(${book3})` }}
+            ></div>
+            <div
+              className="book-item"
+              style={{ backgroundImage: `url(${book4})` }}
+            ></div>
+            <div
+              className="book-item"
+              style={{ backgroundImage: `url(${book1})` }}
+            ></div>
+            <div
+              className="book-item"
+              style={{ backgroundImage: `url(${book2})` }}
+            ></div>
+            <div
+              className="book-item"
+              style={{ backgroundImage: `url(${book3})` }}
+            ></div>
+            <div
+              className="book-item"
+              style={{ backgroundImage: `url(${book4})` }}
+            ></div>
+            <div
+              className="book-item"
+              style={{ backgroundImage: `url(${book1})` }}
+            ></div>
+            <div
+              className="book-item"
+              style={{ backgroundImage: `url(${book2})` }}
+            ></div>
+            <div
+              className="book-item"
+              style={{ backgroundImage: `url(${book3})` }}
+            ></div>
+            <div
+              className="book-item"
+              style={{ backgroundImage: `url(${book4})` }}
+            ></div>
+            <div
+              className="book-item"
+              style={{ backgroundImage: `url(${book1})` }}
+            ></div>
+            <div
+              className="book-item"
+              style={{ backgroundImage: `url(${book2})` }}
+            ></div>
+            <div
+              className="book-item"
+              style={{ backgroundImage: `url(${book3})` }}
+            ></div>
+            <div
+              className="book-item"
+              style={{ backgroundImage: `url(${book4})` }}
+            ></div>
+          </div>
         </div>
       </div>
     );
