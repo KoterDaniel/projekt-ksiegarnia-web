93917ca84aff0790bd9f4ce081500584d0473a24
kodzi97 <64788254+kodzi97@users.noreply.github.com>
2021-05-24 17:27:54 +0200

    Add files via upload

--- Front/reactbookstore/src/components/news.component.js
+++ Front/reactbookstore/src/components/news.component.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 
 import UserService from "../services/user.service";
+import book1 from "../assets/book1.jpg";
+import book2 from "../assets/book2.jpg";
 
 export default class News extends Component {
   constructor(props) {
@@ -8,6 +10,8 @@ export default class News extends Component {
 
     this.state = {
       content: "",
+      daneUzytkownika: "Dane użytkownika",
+      historiaZakupow: "Historia zakupów",
     };
   }
 
@@ -36,6 +40,39 @@ export default class News extends Component {
           <h3>Nowości</h3>
           <h3>{this.state.content}</h3>
         </header>
+
+        <div className="row">
+          <div className="col-lg-6 col-md-6">
+            <p class="h4">{this.state.historiaZakupow}</p>
+          </div>
+          <div className="col-lg-6 col-md-6">
+            <p class="h4">{this.state.daneUzytkownika}</p>
+          </div>
+        </div>
+
+        <div className="row my-5">
+          <div className="col-lg-6 col-md-6 col-sm-12">
+            <p class="font-weight-normal h5">Tytuł: </p>
+            <p class="font-weight-normal h5">data zakupu: </p>
+            <p class="font-weight-normal h5">cena zakupu: </p>
+            <div
+              className="book-item mb-5"
+              style={{ backgroundImage: `url(${book1})` }}
+            ></div>
+
+            <p class="font-weight-normal h5">Tytuł: </p>
+            <p class="font-weight-normal h5">data zakupu: </p>
+            <p class="font-weight-normal h5">cena zakupu: </p>
+            <div
+              className="book-item mb-5"
+              style={{ backgroundImage: `url(${book2})` }}
+            ></div>
+          </div>
+          <div className="col-lg-6 col-md-6 col-sm-12">
+            <p class="font-weight-normal h5">Nick: </p>
+            <p class="font-weight-normal h5">Email: </p>
+          </div>
+        </div>
       </div>
     );
   }
